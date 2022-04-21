bb43cba5249d5ea01ea5928634278815fe4adced
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-04-21 20:43:01 +0300

    added task2 impl

---
+++ task_2.js
@@ -0,0 +1,10 @@
+function first_non_repeating_letter(str) {
+  const lowerCaseStr = str.toLowerCase();
+  for (let i = 0; i < lowerCaseStr.length; i++) {
+    const newRegEx = new RegExp(lowerCaseStr[i], "g");
+    const count = (lowerCaseStr.match(newRegEx) || []).length;
+    if (count === 1) {
+      return str[i];
+    }
+  }
+}
