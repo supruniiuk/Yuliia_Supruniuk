01a72fe659ecc13fa8c444af2431176fd2d979bd
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-04-21 21:52:22 +0300

    added task5 impl

---
+++ task_5.js
@@ -0,0 +1,27 @@
+function sortByFullName(arr) {
+  return arr.sort(function (a, b) {
+    if (a.surname == b.surname) {
+      return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+    } else {
+      return a.surname < b.surname ? -1 : 1;
+    }
+  });
+}
+
+function meeting(str) {
+  let friends = [];
+  let separatedNames = str.toUpperCase().split(";");
+  let resultStr = "";
+
+  separatedNames.forEach((el) => {
+    const fullName = el.split(":");
+    friends.push({ name: fullName[0], surname: fullName[1] });
+  });
+
+  friends = sortByFullName(friends);
+  friends.forEach((friend) => {
+    resultStr += `(${friend.surname}, ${friend.name})`;
+  });
+
+  return resultStr;
+}
