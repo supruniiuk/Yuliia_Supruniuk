75d9c1ad501af6a47549ca134e2aa5cfbff1594f
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-05-04 20:42:50 +0300

    added function calls for examples in task1

--- task_1.js
+++ task_1.js
@@ -1,3 +1,7 @@
 function filter_list(arr) {
   return arr.filter((el) => typeof el !== "string");
 }
+
+console.log(filter_list([1, 2, "a", "b"])); // [1,2]
+console.log(filter_list([1, "a", "b", 0, 15])); // [1,0,15]
+console.log(filter_list([1, 2, "aasf", "1", "123", 123])); // [1,2,123]
